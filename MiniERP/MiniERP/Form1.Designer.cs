db048841af67bff71ffd2bcdfd6ff1126c75a1ec
JSU <totalrook@gmail.com>
2019-01-28 15:40:34 +0900

    메뉴버튼 이벤트 통합처리

--- MiniERP/MiniERP/Form1.Designer.cs
+++ MiniERP/MiniERP/Form1.Designer.cs
@@ -33,22 +33,22 @@
             this.panel_MesMap = new System.Windows.Forms.Panel();
             this.tabMenu = new System.Windows.Forms.TabControl();
             this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btn_Enroll = new System.Windows.Forms.Button();
             this.menu_1 = new System.Windows.Forms.Splitter();
             this.tabPage4 = new System.Windows.Forms.TabPage();
-            this.menu_2 = new System.Windows.Forms.Splitter();
-            this.btn_Enroll = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.panel_Enroll = new System.Windows.Forms.Panel();
             this.button6 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button9 = new System.Windows.Forms.Button();
-            this.button10 = new System.Windows.Forms.Button();
+            this.menu_2 = new System.Windows.Forms.Splitter();
+            this.panel_Enroll = new System.Windows.Forms.Panel();
             this.button11 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
             this.splitContainer2.Panel1.SuspendLayout();
             this.splitContainer2.Panel2.SuspendLayout();
@@ -132,6 +132,56 @@
             this.tabPage3.Text = "재고 I";
             this.tabPage3.UseVisualStyleBackColor = true;
             // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(362, 6);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(83, 25);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "기타 이동";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.btn_Enroll_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(273, 6);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(83, 25);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "생산";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.btn_Enroll_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(184, 6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 25);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "구매 관리";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btn_Enroll_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(95, 6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 25);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "영업 관리";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btn_Enroll_Click);
+            // 
+            // btn_Enroll
+            // 
+            this.btn_Enroll.Location = new System.Drawing.Point(6, 6);
+            this.btn_Enroll.Name = "btn_Enroll";
+            this.btn_Enroll.Size = new System.Drawing.Size(83, 25);
+            this.btn_Enroll.TabIndex = 1;
+            this.btn_Enroll.Text = "기초등록";
+            this.btn_Enroll.UseVisualStyleBackColor = true;
+            this.btn_Enroll.Click += new System.EventHandler(this.btn_Enroll_Click);
+            // 
             // menu_1
             // 
             this.menu_1.Dock = System.Windows.Forms.DockStyle.Top;
@@ -153,6 +203,15 @@
             this.tabPage4.Text = "재고 II";
             this.tabPage4.UseVisualStyleBackColor = true;
             // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(6, 6);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(83, 25);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "A/S 관리";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
             // menu_2
             // 
             this.menu_2.Dock = System.Windows.Forms.DockStyle.Top;
@@ -161,52 +220,6 @@
             this.menu_2.Size = new System.Drawing.Size(471, 31);
             this.menu_2.TabIndex = 1;
             this.menu_2.TabStop = false;
-            // 
-            // btn_Enroll
-            // 
-            this.btn_Enroll.Location = new System.Drawing.Point(6, 6);
-            this.btn_Enroll.Name = "btn_Enroll";
-            this.btn_Enroll.Size = new System.Drawing.Size(83, 25);
-            this.btn_Enroll.TabIndex = 1;
-            this.btn_Enroll.Text = "기초등록";
-            this.btn_Enroll.UseVisualStyleBackColor = true;
-            this.btn_Enroll.Click += new System.EventHandler(this.btn_Enroll_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(95, 6);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(83, 25);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "영업 관리";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(184, 6);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(83, 25);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "구매 관리";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(273, 6);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(83, 25);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "생산";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(362, 6);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(83, 25);
-            this.button5.TabIndex = 5;
-            this.button5.Text = "기타 이동";
-            this.button5.UseVisualStyleBackColor = true;
             // 
             // panel_Enroll
             // 
@@ -222,50 +235,14 @@
             this.panel_Enroll.Size = new System.Drawing.Size(226, 503);
             this.panel_Enroll.TabIndex = 0;
             // 
-            // button6
+            // button11
             // 
-            this.button6.Location = new System.Drawing.Point(6, 6);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(83, 25);
-            this.button6.TabIndex = 2;
-            this.button6.Text = "A/S 관리";
-            this.button6.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(3, 90);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(223, 28);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "거래처 등록";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(3, 132);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(223, 28);
-            this.button7.TabIndex = 1;
-            this.button7.Text = "창고 등록";
-            this.button7.UseVisualStyleBackColor = true;
-            // 
-            // button8
-            // 
-            this.button8.Location = new System.Drawing.Point(3, 174);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(223, 28);
-            this.button8.TabIndex = 2;
-            this.button8.Text = "품목 등록";
-            this.button8.UseVisualStyleBackColor = true;
-            // 
-            // button9
-            // 
-            this.button9.Location = new System.Drawing.Point(3, 216);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(223, 28);
-            this.button9.TabIndex = 3;
-            this.button9.Text = "품목별 단가";
-            this.button9.UseVisualStyleBackColor = true;
+            this.button11.Location = new System.Drawing.Point(3, 300);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(223, 28);
+            this.button11.TabIndex = 5;
+            this.button11.Text = "A/S등록";
+            this.button11.UseVisualStyleBackColor = true;
             // 
             // button10
             // 
@@ -276,14 +253,41 @@
             this.button10.Text = "사원(담당) 등록";
             this.button10.UseVisualStyleBackColor = true;
             // 
-            // button11
+            // button9
             // 
-            this.button11.Location = new System.Drawing.Point(3, 300);
-            this.button11.Name = "button11";
-            this.button11.Size = new System.Drawing.Size(223, 28);
-            this.button11.TabIndex = 5;
-            this.button11.Text = "A/S등록";
-            this.button11.UseVisualStyleBackColor = true;
+            this.button9.Location = new System.Drawing.Point(3, 216);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(223, 28);
+            this.button9.TabIndex = 3;
+            this.button9.Text = "품목별 단가";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(3, 174);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(223, 28);
+            this.button8.TabIndex = 2;
+            this.button8.Text = "품목 등록";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(3, 132);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(223, 28);
+            this.button7.TabIndex = 1;
+            this.button7.Text = "창고 등록";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(223, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "거래처 등록";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
